444fcd9cf5cf3ea822923786b49bcc98825ed871
chl <seacheng@foxmail.com>
2023-10-01 09:19:52 +0800

    版本一

---
+++ model1/src/main/java/Test.java
@@ -0,0 +1,30 @@
+/**
+ * @author chl
+ * @date 2023/10/01
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        String name = "asd";
+        System.out.println(name);
+
+
+        if (10 == 10) {
+            System.out.println(100);
+        }
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ model1/src/test/java/TestTest.java
@@ -0,0 +1,7 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class TestTest {
+    public static void main(String[] args) {
+
+    }
+}
